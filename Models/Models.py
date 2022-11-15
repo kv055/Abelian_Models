e5fca0c2cd1ee2d92ef9fbc56cf6ff90b7ac2515
Kilian Voss <kilian96@live.de>
2022-11-15 16:49:49 -0500

    refactored init, renamed self.data_frame_instance

--- Models/Models.py
+++ Models/Models.py
@@ -4,36 +4,41 @@
 # import statsmodels.api as sm
 import numpy as np
 from Models.PriceData2DataFrame import Creator
+from PriceData.Get_Price_Data_Class import Import_Price_Data
 
 class Modelz():
     """Class"""
-    def __init__(self,StatisticsPriceData) -> None:
-        self.data_frame_instance = Creator(StatisticsPriceData)
-        self.Dataframe = self.data_frame_instance.PriceData
+    def __init__(self, config_dict) -> None:
+        self.price_data_instance = Import_Price_Data()
+        self.list_of_asset_dicts = config_dict['OHLCConfig']
+        self.model_config = config_dict['ModelConfig']
+        self.data_frame_instance = self.price_data_instance.return_merged_average_price_data_frames(
+            self.list_of_asset_dicts
+            )
 
     def rates_of_logarithmic_volatility(self):
         """Method"""
-        index_list = list(self.Dataframe.index.values)
+        index_list = list(self.data_frame_instance.index.values)
         renedered_model = []
-        for rowName, rowData in self.Dataframe.iteritems():
-            self.Dataframe[rowName] = np.log(self.Dataframe[rowName] / self.Dataframe[rowName].shift(1))
-            list_column = self.Dataframe[rowName].values.tolist()
+        for rowName, rowData in self.data_frame_instance.iteritems():
+            self.data_frame_instance[rowName] = np.log(self.data_frame_instance[rowName] / self.data_frame_instance[rowName].shift(1))
+            list_column = self.data_frame_instance[rowName].values.tolist()
             renedered_model.append({'Asset': rowName, 'Value': list_column, 'Time': index_list })
         return renedered_model
 
     def rates_of_simple_volatility(self):
         """Method"""
-        index_list = list(self.Dataframe.index.values)
+        index_list = list(self.data_frame_instance.index.values)
         renedered_model = []
-        for rowName, rowData in self.Dataframe.iteritems():
-            self.Dataframe[rowName] = (self.Dataframe[rowName] / self.Dataframe[rowName].shift(1)) - 1
-            list_column = self.Dataframe[rowName].values.tolist()
+        for rowName, rowData in self.data_frame_instance.iteritems():
+            self.data_frame_instance[rowName] = (self.data_frame_instance[rowName] / self.data_frame_instance[rowName].shift(1)) - 1
+            list_column = self.data_frame_instance[rowName].values.tolist()
             renedered_model.append({'Asset': rowName, 'Value': list_column, 'Time': index_list })
         return renedered_model
 
     def rates_of_return(self):
         """Method"""
-        normalized_list = (self.Dataframe / self.Dataframe.iloc[0] * 100)
+        normalized_list = (self.data_frame_instance / self.data_frame_instance.iloc[0] * 100)
         index_list = list(normalized_list.index.values)
         renedered_model = []
         for column in normalized_list:
@@ -44,11 +49,11 @@ class Modelz():
 
     def rates_of_deviation(self):
         """Method"""
-        index_list = list(self.Dataframe.index.values)
+        index_list = list(self.data_frame_instance.index.values)
         renedered_model = []
-        for rowName, rowData in self.Dataframe.iteritems():
-            self.Dataframe[rowName] = np.log(self.Dataframe[rowName] / self.Dataframe[rowName].shift(1))
-            list_column = self.Dataframe[rowName].values.tolist()
+        for rowName, rowData in self.data_frame_instance.iteritems():
+            self.data_frame_instance[rowName] = np.log(self.data_frame_instance[rowName] / self.data_frame_instance[rowName].shift(1))
+            list_column = self.data_frame_instance[rowName].values.tolist()
             renedered_model.append({'Asset': rowName, 'Value': list_column, 'Time': index_list })
         return renedered_model
 
@@ -57,12 +62,12 @@ class Modelz():
     #     pass
 
 
-        # for rowName, rowData in self.Dataframe.iteritems():
-    #     self.Dataframe['Simple Return'] = (self.Dataframe[rowName] / self.Dataframe[rowName].shift(1)) - 1
-    #     self.Dataframe['Logarithmic Return'] = np.log(self.Dataframe[rowName] / self.Dataframe[rowName].shift(1))
-    #     self.Jochen = self.Dataframe.values.tolist()
+        # for rowName, rowData in self.data_frame_instance.iteritems():
+    #     self.data_frame_instance['Simple Return'] = (self.data_frame_instance[rowName] / self.data_frame_instance[rowName].shift(1)) - 1
+    #     self.data_frame_instance['Logarithmic Return'] = np.log(self.data_frame_instance[rowName] / self.data_frame_instance[rowName].shift(1))
+    #     self.Jochen = self.data_frame_instance.values.tolist()
     #     self.dictToReturn.append(self.Jochen)
     #     print('Jochen', self.Jochen)
-        # print(rowName+' Simple Return raw: ',self.Dataframe['Simple Return'])
-        # print(rowName+' Logarithmic Return raw: ',self.Dataframe['Logarithmic Return'])
+        # print(rowName+' Simple Return raw: ',self.data_frame_instance['Simple Return'])
+        # print(rowName+' Logarithmic Return raw: ',self.data_frame_instance['Logarithmic Return'])
 
