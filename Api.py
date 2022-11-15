08ecba04c144e116b02f71f5e93fab4e368d50b9
Kilian Voss <kilian96@live.de>
2022-11-15 16:44:31 -0500

    Reduced code in the /Statistics Route, has been outsourced to  other modules

--- Api.py
+++ Api.py
@@ -18,23 +18,10 @@ api = Api(app)
 @app.route('/Statistics', methods=['POST'])
 def statistics():
     """Return an ex-parrot."""
-    data = request.get_json()
-    ohlc_config = data['OHLCConfig']
-    model_config = data['ModelConfig']
-    StatisticsPriceData = []
-    get_OHLC_db = Querry_Assets_OHLC_from_DB()
-    for config_set in ohlc_config:
-        ohlc_data = get_OHLC_db.return_historical_ohlc_from_db(config_set['assetPair'])
-        average_price_data = get_OHLC_db.return_historical_average_from_db(config_set['assetPair'])
-        StatisticsPriceData.append({
-            'config':config_set,
-            'OHLC': ohlc_data,
-            'Average': average_price_data
-        })
-        
-    frame = Modelz(StatisticsPriceData)
-    rendered_model = ModelData(model_config,frame)
-    return {'Config': model_config, 'StatisticsModel': rendered_model}
+    Models_Config_Data = request.get_json()
+    frame = Modelz(Models_Config_Data)
+    rendered_model = ModelData(Models_Config_Data,frame)
+    return {'Config': Models_Config_Data, 'StatisticsModel': rendered_model}
 
 # host='0.0.0.0'
 # app.run(host='localhost',port=5001,debug=True)
